89b2b96c5074e36665bfe6fc00add6fca5f1fd03
FelineJTD <13520050@std.stei.itb.ac.id>
2023-04-19 22:50:55 +0700

    refactor: add ui.js

---
+++ src/scripts/ui.js
@@ -0,0 +1,182 @@
+function updateRotationUI() {
+    document.getElementById("rotationX").value = Math.round(state.transform.rotation.x);
+    document.getElementById("rotationX").nextElementSibling.value = Math.round(state.transform.rotation.x);
+    document.getElementById("rotationY").value = Math.round(state.transform.rotation.y);
+    document.getElementById("rotationY").nextElementSibling.value = Math.round(state.transform.rotation.y);
+    document.getElementById("rotationZ").value = Math.round(state.transform.rotation.z);
+    document.getElementById("rotationZ").nextElementSibling.value = Math.round(state.transform.rotation.z);
+}
+
+function updateUI() {
+    document.getElementById("dog").checked = true;
+
+    document.getElementById("color").value = rgbToHexColor(state.color);
+
+    if (state.projection == "orth") document.getElementById("orth").checked = true;
+    if (state.projection == "obliq") document.getElementById("obliq").checked = true;
+    if (state.projection == "persp") document.getElementById("persp").checked = true;
+
+    updateRotationUI();
+
+    document.getElementById("translationX").value = state.transform.translation.x;
+    document.getElementById("translationX").nextElementSibling.value = state.transform.translation.x;
+    document.getElementById("translationY").value = state.transform.translation.y;
+    document.getElementById("translationY").nextElementSibling.value = state.transform.translation.y;
+    document.getElementById("translationZ").value = state.transform.translation.z;
+    document.getElementById("translationZ").nextElementSibling.value = state.transform.translation.z;
+
+    document.getElementById("scalingX").value = state.transform.scale.x;
+    document.getElementById("scalingX").nextElementSibling.value = state.transform.scale.x;
+    document.getElementById("scalingY").value = state.transform.scale.y;
+    document.getElementById("scalingY").nextElementSibling.value = state.transform.scale.y;
+    document.getElementById("scalingZ").value = state.transform.scale.z;
+    document.getElementById("scalingZ").nextElementSibling.value = state.transform.scale.z;
+
+    document.getElementById("cameraRadius").value = state.view.radius;
+    document.getElementById("cameraRadius").nextElementSibling.innerHTML = state.view.radius.toFixed(3);
+
+    document.getElementById("cameraAngle").value = state.view.rotation;
+    document.getElementById("cameraAngle").nextElementSibling.innerHTML = state.view.rotation.toFixed(3);
+
+    document.getElementById("isShadingOn").checked = state.enableShader;
+    document.getElementById("isAnimationOn").checked = state.enableAnimation;
+
+    updateComponentsUI();
+}
+
+function addButton(innerHtml, model, indent, idx) {
+    innerHtml += "<button class='component-button' style='margin-left: " + indent + "em' onclick='state.selectedNode = " + idx + "'>" + model.name + "</button>";
+    console.log(idx);
+    idx++;
+    if (model.children) {
+        model.children.forEach(child => {
+            innerHtml += addButton("", child, indent + 1, idx);
+        });
+    }
+    return innerHtml;
+}
+
+function updateComponentsUI() {
+    const container = document.getElementById("components-container");
+    container.innerHTML = "";
+    // render button for each component and children recursively
+    container.innerHTML += addButton(container.innerHTML, state.model, 0, 0);
+}
+
+function setListeners() {
+    document.getElementById("reset").addEventListener("click", () => {
+        setDefaultState();
+    });
+
+    document.getElementById("dog").oninput = () => {
+        state.model = Dog();
+    };
+
+    document.getElementById("orth").onclick = () => {
+        state.projection = "orth";
+    };
+
+    document.getElementById("obliq").onclick = () => {
+        state.projection = "obliq";
+    };
+
+    document.getElementById("persp").onclick = () => {
+        state.projection = "persp";
+    };
+
+    document.getElementById("rotationX").addEventListener("input", (event) => {
+        // state.transform.rotation.x = Math.round(event.target.value);
+        console.log(state.model.getSelectedModel(state.selectedNode));
+        rotateModelX(state.model.getSelectedModel(state.selectedNode), Math.round(event.target.value));
+        // else
+        // {
+        //     state.model.children[state.selectedNode - 1].transform.rotation.x = Math.round(event.target.value);
+        // }
+    });
+
+    document.getElementById("rotationY").addEventListener("input", (event) => {
+        // state.transform.rotation.y = Math.round(event.target.value);
+        rotateModelY(state.model.getSelectedModel(state.selectedNode), Math.round(event.target.value));
+    });
+
+    document.getElementById("rotationZ").addEventListener("input", (event) => {
+        // state.transform.rotation.z = Math.round(event.target.value);
+        rotateModelZ(state.model.getSelectedModel(state.selectedNode), Math.round(event.target.value));
+    });
+
+    document.getElementById("translationX").addEventListener("input", (event) => {
+        // state.transform.translation.x = event.target.value;
+        translateModelX(state.model.getSelectedModel(state.selectedNode), event.target.value);
+    });
+
+    document.getElementById("translationY").addEventListener("input", (event) => {
+        translateModelY(state.model.getSelectedModel(state.selectedNode), event.target.value);
+    });
+
+    document.getElementById("translationZ").addEventListener("input", (event) => {
+        translateModelZ(state.model.getSelectedModel(state.selectedNode), event.target.value);
+    });
+
+    document.getElementById("scalingX").addEventListener("input", (event) => {
+        scaleModelX(state.model.getSelectedModel(state.selectedNode), event.target.value);
+    });
+
+    document.getElementById("scalingY").addEventListener("input", (event) => {
+        scaleModelY(state.model.getSelectedModel(state.selectedNode), event.target.value);
+    });
+
+    document.getElementById("scalingZ").addEventListener("input", (event) => {
+        scaleModelZ(state.model.getSelectedModel(state.selectedNode), event.target.value);
+    });
+
+    document.getElementById("cameraRadius").addEventListener("input", (event) => {
+        state.view.radius = Number(event.target.value);
+    });
+
+    document.getElementById("cameraAngle").addEventListener("input", (event) => {
+        state.view.rotation = Number(event.target.value);
+    });
+
+    document.getElementById("isShadingOn").addEventListener("change", (event) => {
+        state.enableShader = event.target.checked;
+    });
+
+    document.getElementById("isAnimationOn").addEventListener("change", (event) => {
+        state.enableAnimation = event.target.checked;
+    });
+
+    document.getElementById("canvas").addEventListener("wheel", (event) => {
+        state.view.radius = Math.max(0, Math.min(1, state.view.radius + event.deltaY / 1000));
+        document.getElementById("cameraRadius").value = state.view.radius;
+        document.getElementById("cameraRadius").nextElementSibling.innerHTML = state.view.radius.toFixed(3);
+    });
+
+    // document.getElementById("canvas").addEventListener("mousedown", (event) => {
+    //     state.mousedown = true;
+    // });
+
+    // document.getElementById("canvas").addEventListener("mouseup", (event) => {
+    //     state.mousedown = false;
+    // });
+
+    // document.getElementById("canvas").addEventListener("mousemove", (event) => {
+    //     if (state.mousedown) {
+    //         state.model.transform.rotation.x = Math.round(state.transform.rotation.x - event.movementY);
+    //         state.model.transform.rotation.y = Math.round(state.transform.rotation.y - event.movementX);
+    //         updateRotationUI();
+    //     }
+    // });
+
+    document.getElementById("save").addEventListener("click", () => {
+        save();
+    });
+
+    document.getElementById("load").oninput = (event) => {
+        load(event.target.files[0]);
+    };
+
+    document.getElementById("color").oninput = (event) => {
+        state.color = hexToRGBColor(event.target.value);
+        state.model.updateColor(state.color);
+    };
+}
--- src/scripts/utilities.js
+++ src/scripts/utilities.js
@@ -12,3 +12,128 @@ function rgbToHexColor(rgb) {
     var b = Math.floor(rgb[2] * 255);
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
+
+function setTransformMatrix(modelNode) {
+    let Tmatrix;
+    Tmatrix = scaleMatrix(
+        modelNode.transform.scale.x, 
+        modelNode.transform.scale.y, 
+        modelNode.transform.scale.z);
+    Tmatrix = multiply(rotateMatrix(
+        modelNode.transform.rotation.x * Math.PI / 180, 
+        modelNode.transform.rotation.y * Math.PI / 180, 
+        modelNode.transform.rotation.z * Math.PI / 180),
+        Tmatrix);
+    Tmatrix = multiply(translationMatrix(
+        modelNode.transform.translation.x, 
+        modelNode.transform.translation.y, 
+        modelNode.transform.translation.z),
+        Tmatrix);
+
+    return Tmatrix;
+}
+
+function setViewMatrix() {
+    let Vmatrix;
+
+    Vmatrix = rotateMatrix(0, state.view.rotation * Math.PI / 180, 0);
+    Vmatrix = multiply(Vmatrix, translationMatrix(0, 0, state.view.radius));
+
+    // change the zoom level
+    if (state.projection == "orth") {
+        Vmatrix[14] = Vmatrix[14] + 0.5;
+    } else if (state.projection == "persp"){
+        Vmatrix[14] = Vmatrix[14] - 1.375;
+    } else {
+        Vmatrix[14] = Vmatrix[14] - 1;
+    }
+    return Vmatrix;
+}
+
+function rotateModelX(model, rotate){
+    model.transform.rotation.x = rotate;
+    // if model.transform.rotation.x != 0 then model.transform.rotation.x += rotate
+    if (model.transform.rotation.x != 0) {
+        model.transform.rotation.x += rotate;
+    }
+    else {
+        model.transform.rotation.x = rotate;
+    }
+    if (model.children){
+        model.children.forEach(child => {
+            rotateModelX(child, rotate);
+        });
+    }
+}
+
+function rotateModelY(model, rotate){
+    model.transform.rotation.y = rotate;
+    if (model.children){
+        model.children.forEach(child => {
+            rotateModelY(child, rotate);
+        });
+    }
+}
+
+function rotateModelZ(model, rotate){
+    model.transform.rotation.z = rotate;
+    if (model.children){
+        model.children.forEach(child => {
+            rotateModelZ(child, rotate);
+        });
+    }
+}
+
+function translateModelX(model, translate){
+    model.transform.translation.x = translate;
+    if (model.children){
+        model.children.forEach(child => {
+            translateModelX(child, translate);
+        });
+    }
+}
+
+function translateModelY(model, translate){
+    model.transform.translation.y = translate;
+    if (model.children){
+        model.children.forEach(child => {
+            translateModelY(child, translate);
+        });
+    }
+}
+
+function translateModelZ(model, translate){
+    model.transform.translation.z = translate;
+    if (model.children){
+        model.children.forEach(child => {
+            translateModelZ(child, translate);
+        });
+    }
+}
+
+function scaleModelX(model, scale){
+    model.transform.scale.x = scale;
+    if (model.children){
+        model.children.forEach(child => {
+            scaleModelX(child, scale);
+        });
+    }
+}
+
+function scaleModelY(model, scale){
+    model.transform.scale.y = scale;
+    if (model.children){
+        model.children.forEach(child => {
+            scaleModelY(child, scale);
+        });
+    }
+}
+
+function scaleModelZ(model, scale){
+    model.transform.scale.z = scale;
+    if (model.children){
+        model.children.forEach(child => {
+            scaleModelZ(child, scale);
+        });
+    }
+}
